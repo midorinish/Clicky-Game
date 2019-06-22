3c5550fa936fea40f4cc35667587d4c558d021ad
Midori <midori@symphonyinteractive.com>
2019-06-22 10:23:03 -0400

    added main content

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Componenet} from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Score from "./components/Score";
+import pups from "./cards.json";
+import "./Apps.css";
 
-function App() {
+class App extends Component {
+  state = {
+    pups,
+    clickedPuppyIds: [],
+    score: 0,
+    goal: 8,
+    status: ""
+  };
+
+  shuffleScoreCard = id => {
+    let clickedPuppyIds = this.state.clickedPuppyIds;
+
+    if(clickedPuppyIds.includes(id)){
+      this.setState({ clickedPuppyIds: [], score: 0, status: "Game Over!"});
+      return;
+    } else {
+      clickedPuppyIds.push(id)
+
+      if (clickedPuppyIds.length === 8){
+        this.setState({score:8, status: "You Win!", clickedPuppyIds: []);
+        console.log("You Win!");
+        return;
+      }
+      this.setState ({ pups, clickedPuppyIds, score: clickedPuppyIds.length, status: " "});
+
+      for (let i = pups.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [pups[i], pups[j] = pups[j], pups[i]];
+      }
+    }
+  }
+render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+          <h1 className="App-title">The Clickster</h1>
+          <p className="App-intro">
+            Try not to click the same image twice!
+          </p>
+        </header>
+        <Score total={this.state.score}
+               goal={8}
+               status={this.state.status}
+               />
+        <Wrapper>
+          {this.state.pups.map(puppy => (
+            <Card
+              shuffleScoreCard={this.shuffleScoreCard}
+              id={puppy.id}
+              key={puppy.id}
+              image={puppy.image}
+            />
+          ))}
+        </Wrapper>
+        <footer>
+          <p>Designed and built by Heather Mathies. You can find the
+          code<a href="https://github.com/hmathies/Clicky-Game" target="_blank" rel="noopener noreferrer"> here</a>.</p>
+        </footer>
     </div>
-  );
-}
 
+    );
+  
+  }
+}
 export default App;
